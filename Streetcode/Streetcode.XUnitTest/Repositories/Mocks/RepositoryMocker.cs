186026ae4f9baabdf48429652ff6255cf9a82eb4
ccotae <ccotae1@gmail.com>
2024-04-03 13:28:20 +0300

    delete mock location

--- Streetcode/Streetcode.XUnitTest/Repositories/Mocks/RepositoryMocker.cs
+++ Streetcode/Streetcode.XUnitTest/Repositories/Mocks/RepositoryMocker.cs
@@ -19,17 +19,6 @@ namespace Streetcode.XUnitTest.Repositories.Mocks
 {
     internal class RepositoryMocker
     {
-        /*public static Mock<ILocationRepository> GetLocationRepositoryMock()
-        {
-            var locations = new List<Location>()
-            {
-                new Location() { Id = 1, Streetname = "First location", TableNumber = 1 },
-                new Location() { Id = 2, Streetname = "Second location", TableNumber = 2 },
-                new Location() { Id = 3, Streetname = "Third location", TableNumber = 3 },
-                new Location() { Id = 4, Streetname = "Fourth location", TableNumber = 4 },
-            }
-        } */
-
         public static Mock<IRepositoryWrapper> GetUsersRepositoryMock()
         {
             var users = new List<User>()
