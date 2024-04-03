99ef8b7430ef4f0aad833db7502e4e7d1144dd6b
Denys Klymovets <den.kli2511@gmail.com>
2024-04-03 10:36:40 +0300

    MediaFolder

--- Streetcode/Streetcode.BLL/MediatR/Media/Audio/Create/CreateAudioCommandValidator.cs
+++ Streetcode/Streetcode.BLL/MediatR/Media/Audio/Create/CreateAudioCommandValidator.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using FluentValidation;
+
 namespace Streetcode.BLL.MediatR.Media.Audio.Create
 {
-	public class CreateAudioCommandValidator
-	{
+	public class CreateAudioCommandValidator : AbstractValidator<CreateAudioCommand>
+    {
+		private readonly int maxTitleLength;
+		private readonly int maxBlobNameLength;
+		private readonly int maxMimeTypeLength;
+
 		public CreateAudioCommandValidator()
 		{
-		}
+			maxTitleLength = 100;
+			maxBlobNameLength = 100;
+			maxMimeTypeLength = 10;
+
+			RuleFor(command => command.Audio.Title)
+                .NotEmpty()
+                .WithMessage("Title is required.")
+                .MaximumLength(maxTitleLength)
+                .WithMessage($"Title length should not be longer than {maxTitleLength} symbols.");
+
+			RuleFor(command => command.Audio.MimeType)
+                .NotEmpty()
+                .WithMessage("Blob Name is required.")
+                .MaximumLength(maxMimeTypeLength)
+                .WithMessage($"Blob Name length should not be longer than {maxMimeTypeLength} symbols.");
+        }
 	}
 }
-
--- Streetcode/Streetcode.BLL/MediatR/Media/Image/Create/CreateImageCommandValidator.cs
+++ Streetcode/Streetcode.BLL/MediatR/Media/Image/Create/CreateImageCommandValidator.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using FluentValidation;
+
 namespace Streetcode.BLL.MediatR.Media.Image.Create
 {
-	public class CreateImageCommandValidator
-	{
+	public class CreateImageCommandValidator : AbstractValidator<CreateImageCommand>
+    {
+		private readonly int maxBlobNameLength;
+		private readonly int maxMimeTypeLength;
+		private readonly int maxTitleLength;
+		private readonly int maxAltLength;
+
 		public CreateImageCommandValidator()
 		{
-		}
+			maxBlobNameLength = 100;
+			maxMimeTypeLength = 10;
+
+			RuleFor(command => command.Image.MimeType)
+                .NotEmpty()
+                .WithMessage("Mime Type is required.")
+                .MaximumLength(maxMimeTypeLength)
+                .WithMessage($"Mime Type length should not be longer than {maxMimeTypeLength} symbols.");
+
+			RuleFor(command => command.Image.Title)
+                .MaximumLength(maxMimeTypeLength)
+                .WithMessage($"Title length should not be longer than {maxTitleLength} symbols.");
+
+			RuleFor(command => command.Image.Alt)
+                .MaximumLength(maxMimeTypeLength)
+                .WithMessage($"Alt length should not be longer than {maxAltLength} symbols.");
+        }
 	}
 }
-
