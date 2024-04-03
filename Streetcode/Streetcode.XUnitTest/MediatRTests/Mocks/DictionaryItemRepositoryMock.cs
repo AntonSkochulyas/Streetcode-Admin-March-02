038e3bcccf55decac5c28120c111dba6fd4687fa
askochul <antonskochulyas@gmail.com>
2024-04-03 12:34:41 +0300

    Remove mocks from repository mocker Add separated Mocks for new entities

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/DictionaryItemRepositoryMock.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="DictionaryItemRepositoryMock.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Streetcode.XUnitTest.MediatRTests.Mocks;
+
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.DAL.Entities.Dictionaries;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
+
+internal partial class RepositoryMocker
+{
+    /// <summary>
+    /// Mocks dictionary item repository.
+    /// </summary>
+    /// <returns>Returns mocked repository. </returns>
+    public static Mock<IRepositoryWrapper> GetDictionaryItemRepositoryMock()
+    {
+        var mockRepo = new Mock<IRepositoryWrapper>();
+
+        var dictionaryItems = new List<DictionaryItem>()
+            {
+                new DictionaryItem { Id = 1, Name = "First Dictionary", Description = "First Description"},
+                new DictionaryItem { Id = 2, Name = "Second Dictionary", Description = "Second Description"},
+                new DictionaryItem { Id = 3, Name = "Third Dictionary", Description = "Third Description"},
+                new DictionaryItem { Id = 4, Name = "Fourth Dictionary", Description = "Fourth Description"},
+            };
+
+        mockRepo.Setup(x => x.DictionaryItemRepository.GetAllAsync(It.IsAny<Expression<Func<DictionaryItem, bool>>>(), It.IsAny<Func<IQueryable<DictionaryItem>, IIncludableQueryable<DictionaryItem, object>>>()))
+            .ReturnsAsync(dictionaryItems);
+
+        mockRepo.Setup(x => x.DictionaryItemRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<DictionaryItem, bool>>>(), It.IsAny<Func<IQueryable<DictionaryItem>, IIncludableQueryable<DictionaryItem, object>>>()))
+            .ReturnsAsync((Expression<Func<DictionaryItem, bool>> predicate, Func<IQueryable<DictionaryItem>, IIncludableQueryable<DictionaryItem, object>> include) =>
+            {
+                return dictionaryItems.FirstOrDefault(predicate.Compile());
+            });
+
+        return mockRepo;
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/InfoBlockRepositoryMock.cs
@@ -0,0 +1,101 @@
+﻿// <copyright file="InfoBlockRepositoryMock.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Streetcode.XUnitTest.MediatRTests.Mocks;
+
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.DAL.Entities.InfoBlocks.Articles;
+using Streetcode.DAL.Entities.InfoBlocks.AuthorsInfoes;
+using Streetcode.DAL.Entities.InfoBlocks;
+using Streetcode.DAL.Entities.InfoBlocks.AuthorsInfoes.AuthorsHyperLinks;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
+
+internal partial class RepositoryMocker
+{
+    /// <summary>
+    /// Mocks info block repository.
+    /// </summary>
+    /// <returns>Returns mocked repository. </returns>
+    public static Mock<IRepositoryWrapper> GetInfoBlockRepositoryMock()
+    {
+        var mockRepo = new Mock<IRepositoryWrapper>();
+
+        var infoBlocks = new List<InfoBlock>()
+            {
+                new InfoBlock
+                {
+                    Id = 1,
+                    ArticleId = 1,
+                    Article = new Article { Id = 1, Text = "First Text", Title = "First Title" },
+                    VideoURL = "www.youtube.com",
+                    AuthorShipId = 1,
+                    AuthorShip = new AuthorShip
+                    {
+                        Id = 1,
+                        Text = "First Text",
+                        AuthorShipHyperLinkId = 1,
+                        AuthorShipHyperLink = new AuthorShipHyperLink { Id = 1, Title = "First Title", URL = "First URL"},
+                    },
+                },
+                new InfoBlock
+                {
+                    Id = 2,
+                    ArticleId = 2,
+                    Article = new Article { Id = 2, Text = "Second Text", Title = "Second Title" },
+                    VideoURL = "www.youtube2.com",
+                    AuthorShipId = 2,
+                    AuthorShip = new AuthorShip
+                    {
+                        Id = 2,
+                        Text = "Second Text",
+                        AuthorShipHyperLinkId = 2,
+                        AuthorShipHyperLink = new AuthorShipHyperLink { Id = 2, Title = "Second Title", URL = "Second URL"},
+                    },
+                },
+                new InfoBlock
+                {
+                    Id = 3,
+                    ArticleId = 3,
+                    Article = new Article { Id = 3, Text = "Third Text", Title = "Third Title" },
+                    VideoURL = "www.youtube3.com",
+                    AuthorShipId = 3,
+                    AuthorShip = new AuthorShip
+                    {
+                        Id = 3,
+                        Text = "Third Text",
+                        AuthorShipHyperLinkId = 3,
+                        AuthorShipHyperLink = new AuthorShipHyperLink { Id = 3, Title = "Third Title", URL = "Third URL"},
+                    },
+                },
+                new InfoBlock
+                {
+                    Id = 4,
+                    ArticleId = 4,
+                    Article = new Article { Id = 4, Text = "Fourth Text", Title = "Fourth Title" },
+                    VideoURL = "www.youtube4.com",
+                    AuthorShipId = 4,
+                    AuthorShip = new AuthorShip
+                    {
+                        Id = 4,
+                        Text = "Fourth Text",
+                        AuthorShipHyperLinkId = 4,
+                        AuthorShipHyperLink = new AuthorShipHyperLink { Id = 4, Title = "Fourth Title", URL = "Fourth URL"},
+                    },
+                },
+            };
+
+        mockRepo.Setup(x => x.InfoBlockRepository.GetAllAsync(It.IsAny<Expression<Func<InfoBlock, bool>>>(), It.IsAny<Func<IQueryable<InfoBlock>, IIncludableQueryable<InfoBlock, object>>>()))
+            .ReturnsAsync(infoBlocks);
+
+        mockRepo.Setup(x => x.InfoBlockRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<InfoBlock, bool>>>(), It.IsAny<Func<IQueryable<InfoBlock>, IIncludableQueryable<InfoBlock, object>>>()))
+            .ReturnsAsync((Expression<Func<InfoBlock, bool>> predicate, Func<IQueryable<InfoBlock>, IIncludableQueryable<InfoBlock, object>> include) =>
+            {
+                return infoBlocks.FirstOrDefault(predicate.Compile());
+            });
+
+        return mockRepo;
+    }
+}
