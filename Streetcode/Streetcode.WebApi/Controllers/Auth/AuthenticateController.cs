d3a82966f23c2613cd32be6c9945bd89c64b742b
Yaroslav <dzisyuk.yari@gmail.com>
2024-04-09 02:20:50 +0300

    added revoke methods

--- Streetcode/Streetcode.WebApi/Controllers/Auth/AuthenticateController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Auth/AuthenticateController.cs
@@ -162,5 +162,37 @@ namespace Streetcode.WebApi.Controllers.Auth
 
             return NoContent();
         }
+
+        [Authorize]
+        [HttpPost]
+        [Route("revoke/{username}")]
+        public async Task<IActionResult> Revoke(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user == null)
+            {
+                return BadRequest("Invalid user name");
+            }
+
+            user.RefreshToken = null;
+            await _userManager.UpdateAsync(user);
+
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("revoke-all")]
+        public async Task<IActionResult> RevokeAll()
+        {
+            var users = _userManager.Users.ToList();
+            foreach (var user in users)
+            {
+                user.RefreshToken = null;
+                await _userManager.UpdateAsync(user);
+            }
+
+            return NoContent();
+        }
     }
 }
