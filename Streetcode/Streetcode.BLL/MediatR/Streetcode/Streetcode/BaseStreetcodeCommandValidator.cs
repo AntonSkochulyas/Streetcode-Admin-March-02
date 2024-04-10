f8155102293b89ef1cc551e4e105af7bd42fbb90
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-04-10 20:03:29 +0300

    Fix in validator

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/BaseStreetcodeCommandValidator.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/BaseStreetcodeCommandValidator.cs
@@ -1,9 +1,9 @@
 ﻿using FluentValidation;
-using Streetcode.BLL.Dto.Streetcode;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.Create;
 
 namespace Streetcode.BLL.MediatR.Streetcode.Streetcode;
 
-public class BaseStreetcodeCommandValidator : AbstractValidator<StreetcodeDto>
+public class BaseStreetcodeCommandValidator : AbstractValidator<CreateStreetcodeCommand>
 {
     public BaseStreetcodeCommandValidator()
     {
@@ -16,7 +16,7 @@ public class BaseStreetcodeCommandValidator : AbstractValidator<StreetcodeDto>
         int maxTitleLength = 100;
         int maxNumberOfTags = 50;
 
-        RuleFor(command => command.Teaser)
+        RuleFor(command => command.Streetcode.Teaser)
             .Custom((teaser, context) =>
             {
                 if(teaser is null)
@@ -35,32 +35,32 @@ public class BaseStreetcodeCommandValidator : AbstractValidator<StreetcodeDto>
                 {
                     if (teaser.Length > maxTeaserLength)
                     {
-                        context.AddFailure($"Teaser length of streetcode must not be longer than {{maxTeaserLength}} symbols.");
+                        context.AddFailure($"Teaser length of streetcode must not be longer than {maxTeaserLength} symbols.");
                     }
                 }
             });
 
-        RuleFor(command => command.TransliterationUrl)
+        RuleFor(command => command.Streetcode.TransliterationUrl)
             .MaximumLength(maxUrlLength)
             .WithMessage($"TransliterationUrl length of streetcode must not be longer than {maxUrlLength} symbols.");
 
-        RuleFor(command => command.ShortDescription)
+        RuleFor(command => command.Streetcode.ShortDescription)
                 .MaximumLength(maxShortSecriptionLength)
                 .WithMessage($"ShortDescription length of streetcode must not be longer than {maxShortSecriptionLength} symbols.");
 
-        RuleFor(command => command.DateString)
+        RuleFor(command => command.Streetcode.DateString)
                 .MaximumLength(maxDateStringLength)
                 .WithMessage($"ShortDescription length of streetcode must not be longer than {maxDateStringLength} symbols.");
 
-        RuleFor(command => command.Alias)
+        RuleFor(command => command.Streetcode.Alias)
                 .MaximumLength(maxAliasLength)
                 .WithMessage($"ShortDescription length of streetcode must not be longer than {maxAliasLength} symbols.");
 
-        RuleFor(command => command.Title)
+        RuleFor(command => command.Streetcode.Title)
                 .MaximumLength(maxTitleLength)
                 .WithMessage($"ShortDescription length of streetcode must not be longer than {maxTitleLength} symbols.");
 
-        RuleFor(command => command.Tags)
+        RuleFor(command => command.Streetcode.Tags)
             .Must(t => t.Count() <= maxNumberOfTags)
             .WithMessage($"Number of tags must not be more than {maxNumberOfTags}.");
     }
