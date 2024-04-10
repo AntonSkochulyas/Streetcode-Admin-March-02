4a5837fd2389a17ac35523cca58b4f2b074143de
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-04-10 16:22:09 +0300

    Added all necessary CRUD endpoints and handlers for ImageMain

---
+++ Streetcode/Streetcode.BLL/MediatR/Media/ImageMain/GetById/GetImageMainByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.Dto.Media.Images;
+
+namespace Streetcode.BLL.MediatR.Media.ImageMain.GetById;
+
+public record GetImageMainByIdQuery(int Id) : IRequest<Result<ImageMainDto>>;
