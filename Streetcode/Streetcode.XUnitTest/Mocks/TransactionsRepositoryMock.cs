fee6c3e4a7ea42d4ff200992dc2e97a04a316dd8
Denys Klymovets <den.kli2511@gmail.com>
2024-04-11 06:34:59 +0300

    TransactionLinkRepositoryTest

---
+++ Streetcode/Streetcode.XUnitTest/Mocks/TransactionsRepositoryMock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
+using Streetcode.DAL.Entities.Transactions;
+
+namespace Streetcode.XUnitTest.Mocks
+{
+	internal partial class RepositoryMocker
+	{
+        public static Mock<IRepositoryWrapper> GetTransactionsRepositoryMock()
+        {
+            var transactions = new List<TransactionLink>
+            {
+                new TransactionLink() { Id = 1, Url = "1Url", UrlTitle = "1UrlTitle", StreetcodeId = 1, Streetcode = new DAL.Entities.Streetcode.StreetcodeContent() },
+                new TransactionLink() { Id = 2, Url = "2Url", UrlTitle = "2UrlTitle", StreetcodeId = 2, Streetcode = new DAL.Entities.Streetcode.StreetcodeContent() },
+                new TransactionLink() { Id = 3, Url = "3Url", UrlTitle = "3UrlTitle", StreetcodeId = 3, Streetcode = new DAL.Entities.Streetcode.StreetcodeContent() },
+                new TransactionLink() { Id = 4, Url = "4Url", UrlTitle = "4UrlTitle", StreetcodeId = 4, Streetcode = new DAL.Entities.Streetcode.StreetcodeContent() },
+            };
+
+            var mockRepo = new Mock<IRepositoryWrapper>();
+
+            mockRepo.Setup(x => x.TransactLinksRepository
+                .GetAllAsync(
+                    It.IsAny<Expression<Func<TransactionLink, bool>>>(),
+                    It.IsAny<Func<IQueryable<TransactionLink>, IIncludableQueryable<TransactionLink, object>>>()))
+                .ReturnsAsync(transactions);
+
+            return mockRepo;
+        }
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/Repositories/Transactions/TransactionLinkRepositoryTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using Streetcode.XUnitTest.Mocks;
+using Xunit;
+
+namespace Streetcode.XUnitTest.Repositories.Transactions
+{
+	public class TransactionLinkRepositoryTest
+	{
+        [Fact]
+        public void Repository_Create_TransactionLinkItem_EqualFirstNames()
+        {
+            // Arrange
+            var mockRepo = RepositoryMocker.GetTransactionsRepositoryMock();
+            var repository = mockRepo.Object.TransactLinksRepository;
+            var transactionLinkItemToAdd = new DAL.Entities.Transactions.TransactionLink()
+            {
+                Id = 5,
+                Url = "1Url",
+                UrlTitle = "1UrlTitle",
+                StreetcodeId = 5,
+                Streetcode = new DAL.Entities.Streetcode.StreetcodeContent()
+            };
+
+            // Act
+            var createdTransactionLinkItem = repository.Create(transactionLinkItemToAdd);
+
+            // Assert
+            createdTransactionLinkItem.Should().BeEquivalentTo(transactionLinkItemToAdd);
+        }
+
+        [Fact]
+        public async Task Repository_GetAllTransactionLinks_ReturnsAllTransactionLinksItems()
+        {
+            // Arrange
+            var mockRepo = RepositoryMocker.GetTransactionsRepositoryMock();
+            var repository = mockRepo.Object.TransactLinksRepository;
+
+            // Act
+            var result = await repository.GetAllAsync(null, null);
+
+            // Assert
+            result.Should().HaveCount(4);
+        }
+
+        [Fact]
+        public void Repository_TransactionLinksItemUpdate()
+        {
+            // Arrange
+            var mockRepo = RepositoryMocker.GetTransactionsRepositoryMock();
+            var repository = mockRepo.Object.TransactLinksRepository;
+            var transactionLinkItemToUpdate = new DAL.Entities.Transactions.TransactionLink()
+            {
+                Id = 5,
+                Url = "1UpUrl",
+                UrlTitle = "1UpUrlTitle",
+                StreetcodeId = 5,
+                Streetcode = new DAL.Entities.Streetcode.StreetcodeContent()
+            };
+
+            // Act
+            var updatedTransactionLinkItem = repository.Update(transactionLinkItemToUpdate);
+
+            // Assert
+            mockRepo.Verify(x => x.TransactLinksRepository.Update(It.IsAny<DAL.Entities.Transactions.TransactionLink>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Repository_DeleteTransactionLink_RemovesFromDatabase()
+        {
+            // Arrange
+            var mockRepo = RepositoryMocker.GetTransactionsRepositoryMock();
+            var repository = mockRepo.Object.TransactLinksRepository;
+            var transactionLinkItemIdToDelete = 1;
+
+            // Act
+            repository.Delete(new DAL.Entities.Transactions.TransactionLink { Id = transactionLinkItemIdToDelete });
+
+            // Assert
+            var deletedTransactionLinkItem = await repository.GetFirstOrDefaultAsync(n => n.Id == transactionLinkItemIdToDelete);
+            deletedTransactionLinkItem.Should().BeNull();
+        }
+    }
+}
