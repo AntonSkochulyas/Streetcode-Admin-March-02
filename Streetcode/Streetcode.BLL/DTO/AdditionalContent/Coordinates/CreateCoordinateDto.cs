f86abaf7404410eb518ce6abb8d3bfe9a2fb44b3
ShpurykGleb <110291304+ShpurykGleb@users.noreply.github.com>
2024-04-25 19:37:09 +0300

    Delete Streetcode/Streetcode.BLL/DTO directory

--- Streetcode/Streetcode.BLL/DTO/AdditionalContent/Coordinates/CreateCoordinateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Streetcode.BLL.Dto.AdditionalContent.Coordinates
-{
-    public class CreateCoordinateDto
-    {
-        public decimal? Latitude { get; set; }
-        public decimal? Longtitude { get; set; }
-    }
-}
--- Streetcode/Streetcode.BLL/DTO/Analytics/CreateStatisticRecordDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Streetcode.BLL.Dto.Analytics
-{
-    public class CreateStatisticRecordDto
-    {
-        public int QrId { get; set; }
-        public string? Address { get; set; }
-        public int StreetcodeCoordinateId { get; set; }
-    }
-}
