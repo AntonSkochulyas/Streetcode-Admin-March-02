8f147202837f873213ca1ea76c8f7079b1c109ca
Gleb Shpuryk <stalkergleb74@gmail.com>
2024-04-25 19:44:05 +0300

    fixed

---
+++ Streetcode/Streetcode.BLL/Dto/InfoBlocks/InfoBlockCreateDto.cs
@@ -0,0 +1,33 @@
+﻿using Streetcode.DAL.Entities.InfoBlocks.Articles;
+using Streetcode.DAL.Entities.InfoBlocks.AuthorsInfoes;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+
+namespace Streetcode.BLL.Dto.InfoBlocks
+{
+    public class InfoBlockCreateDto
+    {
+        private AuthorShip? _authorShip;
+
+        public int ArticleId { get; set; }
+        public Article? Article { get; set; }
+        public int? AuthorShipId { get; set; }
+        public string? VideoURL { get; set; }
+        public int TermId { get; set; }
+        public Term? Term { get; set; }
+
+        public AuthorShip? AuthorShip
+        {
+            get
+            {
+                return _authorShip;
+            }
+            set
+            {
+                if (value?.Text == "Текст підготовлений спільно з ")
+                {
+                    _authorShip = null;
+                }
+            }
+        }
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Dto/Users/RegisterModelDto.cs
@@ -0,0 +1,17 @@
+﻿using Streetcode.DAL.Entities.Users;
+
+namespace Streetcode.BLL.Dto.Users
+{
+    public class RegisterModelDto
+    {
+        public int Id { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Role { get; set; } = UserRoles.User;
+
+        public int UserAdditionalInfoId { get; set; }
+    }
+}
