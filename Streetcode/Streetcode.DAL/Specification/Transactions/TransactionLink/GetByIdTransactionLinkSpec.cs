f332dfc8262d699595ff50c9dc6585ce13f6abcf
ccotae <ccotae1@gmail.com>
2024-04-28 22:40:16 +0300

    change spec

--- Streetcode/Streetcode.DAL/Specification/Transactions/TransactionLink/GetByIdTransactionLinkSpec.cs
+++ Streetcode/Streetcode.DAL/Specification/Transactions/TransactionLink/GetByIdTransactionLinkSpec.cs
@@ -4,11 +4,12 @@ namespace Streetcode.DAL.Specification.Transactions.TransactionLink
 {
     public class GetByIdTransactionLinkSpec : Specification<Entities.Transactions.TransactionLink>, ISingleResultSpecification<Entities.Transactions.TransactionLink>
     {
-        public int Id;
         public GetByIdTransactionLinkSpec(int id)
         {
             Id = id;
             Query.Where(t => t.Id == id);
         }
+
+        public int Id { get; set; }
     }
 }
--- Streetcode/Streetcode.DAL/Specification/Transactions/TransactionLink/GetByStreetcodeIdTransactionLinkSpec.cs
+++ Streetcode/Streetcode.DAL/Specification/Transactions/TransactionLink/GetByStreetcodeIdTransactionLinkSpec.cs
@@ -4,11 +4,12 @@ namespace Streetcode.DAL.Specification.Transactions.TransactionLink
 {
     public class GetByStreetcodeIdTransactionLinkSpec : Specification<Entities.Transactions.TransactionLink>, ISingleResultSpecification<Entities.Transactions.TransactionLink>
     {
-        public int StreetcodeId;
         public GetByStreetcodeIdTransactionLinkSpec(int streetcodeId)
         {
             StreetcodeId = streetcodeId;
             Query.Where(t => t.StreetcodeId == streetcodeId);
         }
+
+        public int StreetcodeId { get; set; }
     }
 }
