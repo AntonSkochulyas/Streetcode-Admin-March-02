dbff9e250d42afa75a967ae77e7220cdac34d51a
Yaroslav <dzisyuk.yari@gmail.com>
2024-04-29 03:42:19 +0300

    added tests for GetBaseImageHandler

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Media/ImageMain/GetBaseImage/GetBaseImageHandlerTest.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using Streetcode.BLL.Interfaces.BlobStorage;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Mapping.Media.Images;
+using Streetcode.BLL.MediatR.Media.Image.GetBaseImage;
+using Streetcode.BLL.MediatR.Media.ImageMain.GetById;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.XUnitTest.Mocks;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Media.ImageMain.GetBaseImage
+{
+    public class GetBaseImageHandlerTest
+    {
+        private readonly Mock<IRepositoryWrapper> _mockRepository;
+        private readonly Mock<ILoggerService> _mockLogger;
+        private readonly Mock<IBlobService> _blobService;
+
+        public GetBaseImageHandlerTest()
+        {
+            _mockRepository = RepositoryMocker.GetImageMainRepositoryMock();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<ImageMainProfile>();
+
+            });
+
+            _mockLogger = new Mock<ILoggerService>();
+
+            _blobService = new Mock<IBlobService>();
+        }
+
+        [Fact]
+        public async Task GetBaseImage_WrongId_IsFailedShouldBeTrue()
+        {
+            // Assert
+            int wrongId = 10;
+            var handler = new GetBaseImageHandler(_blobService.Object, _mockRepository.Object, _mockLogger.Object);
+            var request = new GetBaseImageQuery(wrongId);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            result.IsFailed.Should().BeTrue();
+        }
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Media/ImageMain/GetById/GetImageMainByIdHandlerTest.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using Streetcode.BLL.Dto.Media.Images;
+using Streetcode.BLL.Interfaces.BlobStorage;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Mapping.Media.Images;
+using Streetcode.BLL.MediatR.Media.ImageMain.GetById;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.XUnitTest.Mocks;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Media.ImageMain.GetById
+{
+    public class GetImageMainByIdHandlerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IRepositoryWrapper> _mockRepository;
+        private readonly Mock<ILoggerService> _mockLogger;
+        private readonly Mock<IBlobService> _blobService;
+
+        public GetImageMainByIdHandlerTest()
+        {
+            _mockRepository = RepositoryMocker.GetImageMainRepositoryMock();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<ImageMainProfile>();
+
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+
+            _mockLogger = new Mock<ILoggerService>();
+
+            _blobService = new Mock<IBlobService>();
+        }
+
+        [Fact]
+        public async Task GetImageMainById_WrongId_IsFailedShouldBeTrue()
+        {
+            // Assert
+            int wrongId = 10;
+            var handler = new GetImageMainByIdHandler(_mockRepository.Object, _mapper, _blobService.Object, _mockLogger.Object);
+            var request = new GetImageMainByIdQuery(wrongId);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GetImageMainById_CorrectId_IsSuccessShouldBeTrue()
+        {
+            // Assert
+            int correctId = 1;
+            var handler = new GetImageMainByIdHandler(_mockRepository.Object, _mapper, _blobService.Object, _mockLogger.Object);
+            var request = new GetImageMainByIdQuery(correctId);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GetImageMainById_CorrectId_ResultShouldBeOfTypeImageMainDto()
+        {
+            // Assert
+            int correctId = 1;
+            var handler = new GetImageMainByIdHandler(_mockRepository.Object, _mapper, _blobService.Object, _mockLogger.Object);
+            var request = new GetImageMainByIdQuery(correctId);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            result.Value.Should().BeOfType<ImageMainDto>();
+        }
+    }
+}
